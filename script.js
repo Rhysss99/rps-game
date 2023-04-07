b2024589e2a31a3ddcea2e7b8c31a1840b9c3f22
Rhys Chorlton <rhysca99@gmail.com>
2023-04-07 17:56:38 +0100

    Add game functionality with UI

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
+//score
 
-let result = 0;
-function getPlayerSelection(){
+const playerScoreCard = document.querySelector('.playerScoreCard');
+const computerScoreCard = document.querySelector('.computerScoreCard');
+
+let playerPoints = 0;
+let computerPoints = 0;
+let roundNum = 1;
+
+function updatePoints(){
+    playerScoreCard.innerText = String(playerPoints);
+    computerScoreCard.innerText = String(computerPoints);
+}
+
+//Adding querySelectors to buttons
+const playerRock = document.querySelector('.player-rock');
+const playerPaper = document.querySelector('.player-paper');
+const playerScissors = document.querySelector('.player-scissors');
+
+const computerRock = document.querySelector('.computer-rock');
+const computerPaper = document.querySelector('.computer-paper');
+const computerScissors = document.querySelector('.computer-scissors');
+
+// User's Button Event Listener for :hover state
+playerRock.addEventListener('mouseover', playerHover);
+playerPaper.addEventListener('mouseover', playerHover);
+playerScissors.addEventListener('mouseover', playerHover);
+
+//Allow user to hover over buttons if game is still in play (less than 5 points)
+function playerHover() {
+    if(playerPoints<=4 && computerPoints<=4){
+        this.classList.add('playerHover');
+    }
+}
+
+
+/*function getPlayerSelection(){
     playerSelection = prompt('Please enter rock, paper or scissors', '').toLowerCase();
     if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
         console.log(`You chose ${playerSelection}.`);
@@ -8,7 +42,7 @@ function getPlayerSelection(){
     } else {
         console.log(`${playerSelection} is an invalid word.`);
     }
-}
+}*/
 
 
 function getComputerSelection(){
@@ -16,27 +50,31 @@ function getComputerSelection(){
     return computerSelection = optionsArray[Math.floor(Math.random()*optionsArray.length)] 
 }
 
-function playRound(){
-    getPlayerSelection();
+function playRound(playerSelection, computerSelection){
+    playerSelection=this.dataset.button;
+    computerSelection=getComputerSelection();
     //playerSelection = prompt("Rock, Paper or Scissors", "").toLowerCase();
+    this.classList.add('playerClick');
+    updatePoints();
     
-    
-    if (playerSelection == getComputerSelection()){
-        result = 0
-        return "tie";
+    if (playerSelection == computerSelection){
+        roundNum++;
     } else if (playerSelection == "rock" && getComputerSelection() == "scissors" ||
                playerSelection == "paper" && getComputerSelection() == "rock" ||
-               playerSelection == "scissors" && getComputerSelection() == "paper"){
-        result = 1;     
-        return "win";
-        
-
+               playerSelection == "scissors" && getComputerSelection() == "paper"){     
+        playerPoints++;
+        roundNum++;
     } else {
-        result = -1;
-        return "lose";
+        computerPoints++;
+        roundNum++;
     }
 }
-playRound();
+
+
+// User's Button Event Listener to Play the Game
+playerPaper.addEventListener('click', playRound);
+playerRock.addEventListener('click', playRound);
+playerScissors.addEventListener('click', playRound);
 
 
 

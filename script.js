2e02b69f7b63790a5c080f1c318d891344295861
Rhys Chorlton <rhysca99@gmail.com>
2023-04-07 18:52:08 +0100

    Some cleanup

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //score
-
 const playerScoreCard = document.querySelector('.playerScoreCard');
 const computerScoreCard = document.querySelector('.computerScoreCard');
 
@@ -11,7 +10,6 @@ function updatePoints(){
     playerScoreCard.innerText = String(playerPoints);
     computerScoreCard.innerText = String(computerPoints);
 }
-
 //Adding querySelectors to buttons
 const playerRock = document.querySelector('.player-rock');
 const playerPaper = document.querySelector('.player-paper');
@@ -21,18 +19,19 @@ const computerRock = document.querySelector('.computer-rock');
 const computerPaper = document.querySelector('.computer-paper');
 const computerScissors = document.querySelector('.computer-scissors');
 
-// User's Button Event Listener for :hover state
+// players Event Listener to add :hover state
 playerRock.addEventListener('mouseover', playerHover);
 playerPaper.addEventListener('mouseover', playerHover);
 playerScissors.addEventListener('mouseover', playerHover);
 
-//Allow user to hover over buttons if game is still in play (less than 5 points)
-function playerHover() {
+//Allow player to hover over buttons if game is still in play (less than 5 points)
+/*function playerHover() {
     if(playerPoints<=4 && computerPoints<=4){
         this.classList.add('playerHover');
     }
-}
-// User's Button Event Listener to remove :hover state
+}*/
+
+// players Event Listener to remove :hover state
 playerRock.addEventListener('mouseleave', removeHover);
 playerPaper.addEventListener('mouseleave', removeHover);
 playerScissors.addEventListener('mouseleave', removeHover);
@@ -40,18 +39,6 @@ playerScissors.addEventListener('mouseleave', removeHover);
 function removeHover(){
     this.classList.remove('playerHover');
 }
-
-
-/*function getPlayerSelection(){
-    playerSelection = prompt('Please enter rock, paper or scissors', '').toLowerCase();
-    if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
-        console.log(`You chose ${playerSelection}.`);
-        return playerSelection;
-    } else {
-        console.log(`${playerSelection} is an invalid word.`);
-    }
-}*/
-
 
 function getComputerSelection(){
     const optionsArray = ['rock', 'paper', 'scissors'];
@@ -71,7 +58,6 @@ function computerAnimation(computerSelection) {
         removeColour();
         computerScissors.classList.add('computerPick');
     }
-
 }
 
 function removeColour() {
@@ -83,10 +69,8 @@ function removeColour() {
 function playRound(playerSelection, computerSelection){
     playerSelection=this.dataset.button;
     computerSelection=getComputerSelection();
-    //playerSelection = prompt("Rock, Paper or Scissors", "").toLowerCase();
     this.classList.add('playerClick');
-    
-    
+        
     if (playerSelection == computerSelection){
         roundNum++;
         updatePoints();
@@ -103,9 +87,7 @@ function playRound(playerSelection, computerSelection){
     }
     computerAnimation(computerSelection);
 }
-
-
-// User's Button Event Listener to Play the Game
+// player Event Listener to Play the Game
 playerPaper.addEventListener('click', playRound);
 playerRock.addEventListener('click', playRound);
 playerScissors.addEventListener('click', playRound);
@@ -124,17 +106,5 @@ computerRock.addEventListener('transitionend', removeColour);
 computerPaper.addEventListener('transitionend', removeColour);
 computerScissors.addEventListener('transitionend', removeColour);
 
-/*function game(){
-    let scores = [0,0];
-    for(let i=0;i<5;i++){
-        playRound();
-        if (result == 1){
-            scores[0]=(scores[0]+1)
-        } else if (result == -1) {
-            scores[1]=(scores[1]+1)
-        }
-    }
-    console.log("Player has a score of:" +scores[0]);
-    console.log("Computer has a score of:" +scores[1]);
-}*/
+
 
